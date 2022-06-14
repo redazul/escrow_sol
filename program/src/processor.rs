781dbec9bb502252d03be2035f30f1e8f61b0212
redazul <azuldevgames@gmail.com>
2022-06-14 13:58:10 -0400

    update

--- program/src/processor.rs
+++ program/src/processor.rs
@@ -34,6 +34,8 @@ impl Processor {
                 msg!("Instruction: Init Escrow");
 
                 //pda 
+                msg!("1st Seed used is {:?}",&[b"escrow"]);
+                msg!("2nd Seed used is {:?}",_program_id);
                 let (pda, _nonce) = Pubkey::find_program_address(&[b"escrow"], _program_id);
                 msg!("Program PDA is {}",pda);
 
@@ -52,69 +54,40 @@ impl Processor {
 
                 let token_program_id = next_account_info(accounts_iter)?;
                 msg!("TokenProgram sent {:?}",token_program_id);
+
+                let pda_account = next_account_info(accounts_iter)?;
+                msg!("PDA sent {:?}",pda_account);
+
+                let system_program = next_account_info(accounts_iter)?;
+                msg!("system_program sent {:?}",system_program);
                 
                 // { key: DgeNUoaYCr5dQrDuCWp1SwgZ4um9NR46xqCNwGyd9EF6, owner: 11111111111111111111111111111111, is_signer: true, is_writable: true, executable: false, rent_epoch: 323, lamports: 25486670200, data.len: 0, .. }
 
                 
-                // //create pda account 
-                // let pda_account_ix = solana_program::system_instruction::create_account(
-                //     feepayer.key, 
-                //     &pda, 
-                //     Rent::get()?.minimum_balance(TokenAccount::LEN), // enough sol to pay for rent 
-                //     TokenAccount::LEN as u64, //space to hold meta data
-                //     _program_id,
-                // );
+                //create pda account 
+                let pda_account_ix = solana_program::system_instruction::create_account(
+                    feepayer.key, 
+                    pda_account.key, 
+                    Rent::get()?.minimum_balance(TokenAccount::LEN), // enough sol to pay for rent 
+                    TokenAccount::LEN as u64, //space to hold meta data
+                    _program_id,
+                );
 
-                // msg!("TokenProgram sent {:?}",pda_account_ix);
+                msg!("TokenProgram sent {:?}",pda_account_ix);
 
-                // let res = solana_program::program::invoke_signed(
-                //     &pda_account_ix,
-                //     &[
-                //         feepayer.clone(),
-                        
-                //     ],
-                //     &[&[b"escrow"],&[&[_nonce]]]
-                // )?;
+                let res = solana_program::program::invoke_signed(
+                    &pda_account_ix,
+                    &[
+                        feepayer.clone(),
+                        pda_account.clone(),
+                        system_program.clone()                   
+                    ],
+                    &[&[b"escrow"],&[&[_nonce]]]
+                )?;
 
-                // invoke_signed(
-                //     &system_instruction::create_account(
-                //         user.key,
-                //         tracker_ai.key,
-                //         Rent::get()?.minimum_balance(42),
-                //         42,
-                //         program_id,
-                //     ),
-                //     &[user.clone(), tracker_ai.clone(), system_program.clone()],
-                //     &[&[user.key.as_ref(), counter.key.as_ref(), &[bump]]],
-                // )?;
+                msg!("Res:  {:?}",res);
 
-                // msg!("Create account response {:?}",res);
-
-
-
-
-                // //build ix
-                // let tx_ix = spl_token::instruction::transfer(
-                //     token_program_id.key,
-                //     spl_token.key,
-                //     &pda,
-                //     feepayer.key,
-                //     &[&feepayer.key],
-                //     amount,
-                // )?;
-
-           
-
-                // msg!("The build instruction {:?}",tx_ix);
-                // solana_program::program::invoke(
-                //     &tx_ix,
-                //     &[
-                //         feepayer.clone(),
-                //         spl_token.clone(),
-                //         token_program_id.clone(),
-                //         &pda
-                //     ],
-                // )?;
+  
 
 
 
