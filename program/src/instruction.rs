6bebab47a40ed17972040f6faf0b28e4df2c237f
azuldev <91290695+redazul@users.noreply.github.com>
2022-11-24 02:47:31 -0500

    Update instruction.rs

--- program/src/instruction.rs
+++ program/src/instruction.rs
@@ -2,5 +2,5 @@ use borsh::{BorshDeserialize, BorshSerialize};
 
 #[derive(BorshSerialize, BorshDeserialize, Debug, Clone)]
 pub enum CounterInstruction {
-    InitEscrow {amount: u64},
+    Increment, // unsigned byte
 }
